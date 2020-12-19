a89c8362699fb1669faacb7348b96d9b12a4661a
SaiRakshithS <75689519+SaiRakshithS@users.noreply.github.com>
2020-12-20 00:58:02 +0530

    Add files via upload

---
+++ src/quaternion.c
@@ -0,0 +1,103 @@
+#include <math.h>
+#include "quaternion.h"
+
+    quaternion conjugateQ (quaternion a)
+
+        {
+            quaternion result ;
+
+            result.re = a.re ;
+            result.im = scaleall (a.im, -1.0) ;
+
+            return result ;
+        }
+
+    double modQ (quaternion a)
+
+        {
+            double result ;
+
+            result = (a.re * a.re) + magnSquare (a.im) ;
+            result = sqrt (result) ;
+
+            return result ;
+        }
+
+    quaternion inverseQ (quaternion a)
+
+        {
+            quaternion result = conjugateQ (a) ;
+
+            result.re = result.re * modQ (a) ;
+            result.im = scaleall(result.im, modQ (a)) ;
+
+            return result ;
+        }
+
+    quaternion addQ (quaternion a, quaternion b)
+
+        {
+            quaternion result ;
+
+            result.re = a.re + b.re ;
+            result.im = add (a.im, b.im) ;
+
+            return result ;
+        }
+
+    quaternion subQ (quaternion a, quaternion b)
+
+        {
+            quaternion result ;
+
+            result.re = a.re - b.re ;
+            result.im = sub (a.im, b.im) ;
+
+            return result ;
+        }
+
+    quaternion mul2Q (quaternion a, quaternion b)
+
+        {
+            quaternion result ;
+
+            result.re = ( a.re * b.re ) - dot (a.im, b.im) ;
+
+            a.im = scaleall (a.im, b.re) ;
+            b.im = scaleall (b.im, a.re) ;
+
+            result.im = add (a.im, b.im) ;
+            result.im = add (result.im, cross (a.im, b.im)) ;
+
+            return result ;
+        }
+
+    quaternion mul3Q (quaternion a, quaternion b, quaternion c)
+
+        {
+            quaternion result ;
+
+            result = mul2Q (a, b) ;
+            result = mul2Q (result, c) ;
+
+            return result ;
+        }
+
+    vect rot (vect a, vect n, double angle)
+
+        {
+            vect result ;
+
+            quaternion pure ;
+            pure.re = 0.0 ;
+            pure.im = a ;
+
+            quaternion rotator ;
+            rotator.re = cos (angle / 2) ;
+            rotator.im = normalize (n) ;
+            rotator.im = scaleall (rotator.im, sin (angle / 2)) ;
+
+            result = mul3Q (rotator, pure, conjugateQ (rotator)).im ;
+
+            return result ;
+        }
---
+++ src/quaternion.h
@@ -0,0 +1,38 @@
+#ifndef QUATERNION_H_
+#define QUATERNION_H_
+
+#include "vector.h"
+
+    typedef struct
+
+        {
+            double re ;
+            vect im ;
+
+        } quaternion ;
+
+    quaternion conjugateQ (quaternion) ;
+
+
+    double modQ (quaternion) ;
+
+
+    quaternion inverseQ (quaternion) ;
+
+
+    quaternion addQ (quaternion, quaternion) ;
+
+
+    quaternion subQ (quaternion, quaternion) ;
+
+
+    quaternion mul2Q (quaternion, quaternion) ;
+
+
+    quaternion mul3Q (quaternion, quaternion, quaternion) ;
+
+
+    vect rot (vect, vect, double) ;
+
+
+#endif
