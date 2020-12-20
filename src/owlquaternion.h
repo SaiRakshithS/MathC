5df797787b07a944337d5e6348bac5c922ab91a7
SaiRakshithS <75689519+SaiRakshithS@users.noreply.github.com>
2020-12-20 12:05:21 +0530

    Update owlquaternion.h

--- src/owlquaternion.h
+++ src/owlquaternion.h
@@ -15,22 +15,22 @@
     quaternion conjugateQ (quaternion) ;                                // CONJUGATE OF QUATERNION //
 
 
-    double modQ (quaternion) ;                                         // MODULUS OF QUATERNION //
+    double modQ (quaternion) ;                                          // MODULUS OF QUATERNION //
 
 
-    quaternion inverseQ (quaternion) ;                                // INVERSE OF QUATERNION //
+    quaternion inverseQ (quaternion) ;                                  // INVERSE OF QUATERNION //
 
 
-    quaternion addQ (quaternion, quaternion) ;                       // ADD 2 QUATERNIONS //
+    quaternion addQ (quaternion, quaternion) ;                          // ADD 2 QUATERNIONS //
 
 
-    quaternion subQ (quaternion, quaternion) ;                      // SUBTRACT 2 QUATERNIONS //
+    quaternion subQ (quaternion, quaternion) ;                          // SUBTRACT 2 QUATERNIONS //
 
 
-    quaternion mul2Q (quaternion, quaternion) ;                     // MULTIPLY 2 QUATERNIONS //
+    quaternion mul2Q (quaternion, quaternion) ;                         // MULTIPLY 2 QUATERNIONS //
 
 
-    quaternion mul3Q (quaternion, quaternion, quaternion) ;        // MULTIPLY 3 QUATERNIONS //
+    quaternion mul3Q (quaternion, quaternion, quaternion) ;             // MULTIPLY 3 QUATERNIONS //
 
 
     quaternion rot (vect, vect, double) ;                               // ROTATE A VECTOR ABOUT A VECTOR BY AN ANGLE //
